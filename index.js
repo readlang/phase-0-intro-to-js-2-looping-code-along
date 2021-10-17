a35d452a99a6c6bafdc6936ca2cf3a58835e9895
Read Langworthy <rlangworthy@gmail.com>
2021-10-17 16:56:21 -0700

    commit tree f046fcd9caab6b2623843080a5a3bfd2adbebfe5

--- index.js
+++ index.js
@@ -6,36 +6,83 @@ for (let age = 30; age < 40; age++) {
 }
 */
 
-const gifts =  ["teddy bear", "drone", "doll"];
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-        //debugger;
+
+function writeCards(arrayOfNames, word) {   
+    let newArr = [];
+    for (const i of arrayOfNames) {
+        newArr.push(`Thank you, ${i}, for the wonderful ${word} gift!`)
     }
-    return gifts;
+    return newArr
 }
 
-wrapGifts(gifts);
 
-
-function writeCards(arrayOfNames, eventName) {
-    const newArrayOfNames = [];
-    for (let counter = 0; counter < arrayOfNames.length; counter++){
-        newArrayOfNames.push(`Thank you, ${arrayOfNames[counter]}, for the wonderful ${eventName} gift!`);
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
     }
-    return newArrayOfNames;
+
 
 }
 
-//console.log(
-writeCards(["Ada", "Brendan", "Ali"], "birthday");
-//);
 
 
-function countDown(posInt) {
-    while (posInt >= 0) {
-        console.log(posInt--);
-    }
-}
 
-countDown(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const gifts =  ["teddy bear", "drone", "doll"];
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         //debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+// function writeCards(arrayOfNames, eventName) {
+//     const newArrayOfNames = [];
+//     for (let counter = 0; counter < arrayOfNames.length; counter++){
+//         newArrayOfNames.push(`Thank you, ${arrayOfNames[counter]}, for the wonderful ${eventName} gift!`);
+//     }
+//     return newArrayOfNames;
+
+// }
+
+// //console.log(
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+// //);
+
+
+// function countDown(posInt) {
+//     while (posInt >= 0) {
+//         console.log(posInt--);
+//     }
+// }
+
+// countDown(10);
